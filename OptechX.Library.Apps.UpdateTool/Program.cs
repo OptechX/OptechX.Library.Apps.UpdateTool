51e1d0f4a4d0ad29a8487826bfcbcecc5c59cd77
Danijel-James Wynyard <danijel@repasscloud.com>
2023-08-04 20:42:37 +1000

    Update to v1.0.3

--- OptechX.Library.Apps.UpdateTool/Program.cs
+++ OptechX.Library.Apps.UpdateTool/Program.cs
@@ -10,7 +10,7 @@ namespace OptechX.Library.Apps.UpdateTool
         {
             if (args.Any(arg => arg.Contains("--version")))
             {
-                Console.WriteLine("Version: 1.0.1");
+                Console.WriteLine("Version: 1.0.3");
                 return;
             }
 
@@ -89,7 +89,7 @@ namespace OptechX.Library.Apps.UpdateTool
                     {
                         HttpResponseMessage updateResponse = await httpClient.PutAsync(apiUpdateApplicationUrl, new StringContent(JsonSerializer.Serialize(uApp), Encoding.UTF8, "application/json"));
                         updateResponse.EnsureSuccessStatusCode();
-                        Console.WriteLine("Application updated");
+                        Console.WriteLine($"Application updated: {uApp.UID}");
                     }
                     catch (HttpRequestException ex)
                     {
